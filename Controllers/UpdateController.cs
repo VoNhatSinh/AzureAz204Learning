f9372c1fd07307d84bc4b27298d2f74f5d6ff337
nsv <sinh.vo@elca.vn>
2025-06-08 11:17:31 +0700

    Add logging

--- Controllers/UpdateController.cs
+++ Controllers/UpdateController.cs
@@ -10,15 +10,17 @@ namespace Azure_Az204.Controllers
     public class UpdateController : ControllerBase
     {
         private readonly IHubContext<MessageHub> _hubContext;
-
-        public UpdateController(IHubContext<MessageHub> hubContext)
+        private readonly ILogger<HomeController> _logger;
+        public UpdateController(IHubContext<MessageHub> hubContext, ILogger<HomeController> logger)
         {
             _hubContext = hubContext;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post()
         {
+            _logger.LogInformation("Post called");
             using var reader = new StreamReader(Request.Body, Encoding.UTF8);
             var jsonContent = await reader.ReadToEndAsync();
             await _hubContext.Clients.All.SendAsync("ReceiveMessage", jsonContent);
